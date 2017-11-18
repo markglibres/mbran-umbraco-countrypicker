42ba406d254064c688b489938279aac8bccb3fb9
Mark Gil Libres <markglibres@gmail.com>
2017-11-19 02:22:13 +1100

    country picker

---
+++ src/CountryApiController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace MBran.Umbraco
+{
+    [PluginController("MBranApi")]
+    public class CountryController : UmbracoApiController
+    {
+        public IEnumerable<Country> GetAll()
+        {
+            var cultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+            var countries = cultures
+                .Select(culture =>
+                    new RegionInfo(culture.LCID)
+                ).Select(region =>
+                    new Country
+                    {
+                        Code = region.TwoLetterISORegionName,
+                        Name = region.EnglishName
+                    }
+                )
+                .OrderBy(country => country.Name)
+                .GroupBy(country => country.Code)
+                .Select(country => country.FirstOrDefault());
+            
+            return countries;
+        }
+    }
+}
---
+++ src/Models/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace MBran.Umbraco
+{
+    public class Country
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
